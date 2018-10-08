807edc2790c8cf4e15a2c9ac96add3e8ad7a967d
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-08 09:52:30 +0700

    Refine Service interface

--- service.go
+++ service.go
@@ -2,14 +2,14 @@ package gotodo
 
 // Service will implement this library's use cases.
 type Service interface {
-	Get(id int) Todo
+	Get(id int) (Todo, error)
 	GetAll() []Todo
 	GetPending() []Todo
 	GetFinished() []Todo
-	Add(title string, description *string) Todo
-	Edit(todo Todo) Todo
+	Add(title string, description *string) (Todo, error)
+	Edit(todo Todo) (Todo, error)
 	MarkAsDone(todo Todo) Todo
-	Delete(todo Todo)
+	Delete(todo Todo) error
 	DeleteFinished()
 }
 
