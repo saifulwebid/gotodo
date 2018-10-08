18230358d0f68ce17e51b0f77e1abcfd584c4880
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-08 10:36:15 +0700

    database: Add Todo struct and Service interface

---
+++ database/todo.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"github.com/saifulwebid/gotodo"
+)
+
+type Todo struct {
+	ID          int    `gorm:"PRIMARY_KEY"`
+	Title       string `gorm:"NOT NULL"`
+	Description *string
+	Done        bool
+}
+
+func FromEntity(todo *gotodo.Todo) Todo {
+	var done bool
+	if todo.Status() == gotodo.Finished {
+		done = true
+	} else {
+		done = false
+	}
+
+	return Todo{
+		ID:          todo.ID(),
+		Title:       todo.Title,
+		Description: todo.Description,
+		Done:        done,
+	}
+}
+
+func (t Todo) AsEntity() gotodo.Todo {
+	var status gotodo.Status
+	if t.Done {
+		status = gotodo.Finished
+	} else {
+		status = gotodo.Pending
+	}
+
+	return gotodo.NewTodo(
+		t.ID,
+		t.Title,
+		t.Description,
+		status,
+	)
+}
