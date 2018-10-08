c541152cb24dd212716097219e8503404d9c41e4
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-08 10:58:45 +0700

    database: Add status argument to Service.Insert()
    And also correct my wrongdoings :(

--- database/service.go
+++ database/service.go
@@ -8,8 +8,8 @@ type Service interface {
 	Get(id int) (*gotodo.Todo, error)
 	GetAll() []gotodo.Todo
 	GetWhere(status gotodo.Status) []gotodo.Todo
-	Insert(title string, description *string) (*gotodo.Todo, error)
-	Update(todo gotodo.Status) error
-	Delete(todo gotodo.Status) error
+	Insert(title string, description *string, status bool) (*gotodo.Todo, error)
+	Update(todo gotodo.Todo) error
+	Delete(todo gotodo.Todo) error
 	DeleteWhere(status gotodo.Status) error
 }
