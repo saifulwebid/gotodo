6bb73b975eb9d26e9cf99c225f0c11e3212f4bdf
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-08 11:10:14 +0700

    database: Remove Service.DeleteWhere return value

--- database/service.go
+++ database/service.go
@@ -8,8 +8,8 @@ type Service interface {
 	Get(id int) (*gotodo.Todo, error)
 	GetAll() []gotodo.Todo
 	GetWhere(status gotodo.Status) []gotodo.Todo
-	Insert(title string, description *string, status bool) (*gotodo.Todo, error)
-	Update(todo gotodo.Todo) error
-	Delete(todo gotodo.Todo) error
-	DeleteWhere(status gotodo.Status) error
+	Insert(title string, description *string, done bool) (*gotodo.Todo, error)
+	Update(entityTodo gotodo.Todo) error
+	Delete(entityTodo gotodo.Todo) error
+	DeleteWhere(status gotodo.Status)
 }
