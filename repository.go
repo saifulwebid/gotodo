acc81052ec34815e0b94f38ce3b943815652ffb3
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-08 16:22:13 +0700

    gotodo: GetAll() and GetWhere() returns []*Todo

--- repository.go
+++ repository.go
@@ -2,8 +2,8 @@ package gotodo
 
 type Repository interface {
 	Get(id int) (*Todo, error)
-	GetAll() []Todo
-	GetWhere(status Status) []Todo
+	GetAll() []*Todo
+	GetWhere(status Status) []*Todo
 	Insert(title string, description *string, done bool) (*Todo, error)
 	Update(entityTodo *Todo) error
 	Delete(entityTodo *Todo) error
