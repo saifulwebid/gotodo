08ed19d90add9292ef79f838cb4fa44d02b3793a
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-08 16:26:40 +0700

    gotodo: Implement Service

--- service.go
+++ service.go
@@ -2,19 +2,59 @@ package gotodo
 
 // Service will implement this library's use cases.
 type Service interface {
-	Get(id int) (Todo, error)
-	GetAll() []Todo
-	GetPending() []Todo
-	GetFinished() []Todo
-	Add(title string, description *string) (Todo, error)
-	Edit(todo Todo) (Todo, error)
-	MarkAsDone(todo Todo) Todo
-	Delete(todo Todo) error
+	Get(id int) (*Todo, error)
+	GetAll() []*Todo
+	GetPending() []*Todo
+	GetFinished() []*Todo
+	Add(title string, description *string) (*Todo, error)
+	Edit(todo *Todo) error
+	MarkAsDone(todo *Todo)
+	Delete(todo *Todo) error
 	DeleteFinished()
 }
 
-type todolistService struct{}
+type service struct {
+	repo Repository
+}
 
-func NewService() Service {
-	return todolistService{}
+func NewService(repo Repository) Service {
+	return &service{
+		repo: repo,
+	}
+}
+
+func (s *service) Get(id int) (*Todo, error) {
+	return s.repo.Get(id)
+}
+
+func (s *service) GetAll() []*Todo {
+	return s.repo.GetAll()
+}
+
+func (s *service) GetPending() []*Todo {
+	return s.repo.GetWhere(Pending)
+}
+
+func (s *service) GetFinished() []*Todo {
+	return s.repo.GetWhere(Finished)
+}
+
+func (s *service) Add(title string, description *string) (*Todo, error) {
+	return s.repo.Insert(title, description, false)
+}
+
+func (s *service) Edit(todo *Todo) error {
+	return s.repo.Update(todo)
+}
+
+func (s *service) MarkAsDone(todo *Todo) {
+	todo.markAsDone()
+}
+
+func (s *service) Delete(todo *Todo) error {
+	return s.repo.Delete(todo)
+}
+
+func (s *service) DeleteFinished() {
+	s.repo.DeleteWhere(Finished)
 }
