32443c737cdb60dd0c3e35cf0edfd20a26a2956d
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-09 13:19:45 +0700

    gotodo: Repository.Insert should receive entityTodo

--- repository.go
+++ repository.go
@@ -4,7 +4,7 @@ type Repository interface {
 	Get(id int) (*Todo, error)
 	GetAll() []*Todo
 	GetWhere(status Status) []*Todo
-	Insert(title string, description string, done bool) (*Todo, error)
+	Insert(entityTodo *Todo) (*Todo, error)
 	Update(entityTodo *Todo) error
 	Delete(entityTodo *Todo) error
 	DeleteWhere(status Status)
