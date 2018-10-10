1afdae9b03e9a42bbe04cd196c1f75da2955489f
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-10 13:32:52 +0700

    gotodo: Add JSON tags to Todo struct

--- todo.go
+++ todo.go
@@ -2,10 +2,10 @@ package gotodo
 
 // Todo is a struct representation of, well, a todo.
 type Todo struct {
-	ID          int    `gorm:"PRIMARY_KEY"`
-	Title       string `gorm:"NOT NULL"`
-	Description string
-	Done        bool `gorm:"NOT NULL"`
+	ID          int    `gorm:"PRIMARY_KEY" json:"id"`
+	Title       string `gorm:"NOT NULL" json:"title"`
+	Description string `json:"description"`
+	Done        bool   `gorm:"NOT NULL" json:"done"`
 }
 
 // IsValid returns true if the todo is valid; false otherwise.
